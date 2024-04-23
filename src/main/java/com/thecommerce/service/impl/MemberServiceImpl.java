a5b620b514b8049bd93ab29a72dccaeae7fe099b
Jason Kwon <jasonproject628@gmail.com>
2024-04-23 17:02:44 +0900

    Feat : MemberServiceImpl 기본 구현 기본 정보 구현

--- src/main/java/com/thecommerce/service/impl/MemberServiceImpl.java
+++ src/main/java/com/thecommerce/service/impl/MemberServiceImpl.java
@@ -1,4 +1,50 @@
 package com.thecommerce.service.impl;
 
-public class MemberServiceImpl {
+import com.thecommerce.domain.dto.MemberDTO;
+import com.thecommerce.domain.entity.Member;
+import com.thecommerce.repository.MemberRepository;
+import com.thecommerce.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Log4j2
+@Transactional
+@Service
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void addOrUpdateMember(MemberDTO memberDTO) {
+        // 회원가입/회원정보수정: C/U
+        Member member = Member.builder()
+                .id(memberDTO.getId())
+                .memberId(memberDTO.getMemberId())
+                .memberPassword(memberDTO.getMemberPassword())
+                .build();
+        memberRepository.save(member);
+    }
+
+    @Override
+    public MemberDTO getMemberById(Long id) {
+        // 나의 회원정보조회: R
+        Member member = memberRepository.findById(id).orElse(null);
+        if (member != null) {
+            return new MemberDTO(
+                    member.getId(),
+                    member.getMemberId(),
+                    member.getMemberPassword()
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteMemberById(Long memberId) {
+        // 회원 탈퇴: D
+        memberRepository.deleteById(memberId);
+    }
 }
